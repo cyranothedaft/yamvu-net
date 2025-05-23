26aaa89d888ccaff25ca9c8b72d4269110cf7f8e
cyranothedaft <cyranothedev@gmail.com>
2025-05-23 00:06:40 -0400

    yamvu.Extensions.WinUI3 wip

--- source/Samples/Apps/WinUI3/WinUI3CounterSample-gui/App.xaml.cs
+++ source/Samples/Apps/WinUI3/WinUI3CounterSample-gui/App.xaml.cs
@@ -25,6 +25,7 @@ using Windows.Foundation.Collections;
 using WinUI3CounterSample.View;
 using yamvu;
 using yamvu.core;
+using yamvu.Extensions.WinUI3;
 using yamvu.Runners;
 
 
@@ -38,10 +39,7 @@ namespace WinUI3CounterSample;
 /// Provides application-specific behavior to supplement the default Application class.
 /// </summary>
 public partial class App : Application {
-
    private const LogLevel MinimumLogLevel = LogLevel.Trace;
-
-   private MainWindow? _mainWindow;
 
 
    /// <summary>
@@ -58,80 +56,23 @@ public partial class App : Application {
    /// </summary>
    /// <param name="args">Details about the launch request and process.</param>
    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args) {
-      _mainWindow = new MainWindow();
-      embedMvuProgramInWindow(_mainWindow);
-      _mainWindow.Activate();
-   }
 
+      using var loggerFactory = LoggerFactory.Create(builder => builder.AddDebug()
+                                                                       .SetMinimumLevel(MinimumLogLevel));
 
-   private static void embedMvuProgramInWindow(MainWindow window) {
-      ExternalMessageDispatcher externalMessageDispatcher = new();
-
-      async void onActivatedRunMvuProgram(object sender, WindowActivatedEventArgs args) {
-         try {
-            // immediately unsubscribe so this only happens once
-            window.Activated -= onActivatedRunMvuProgram;
-
-            using var loggerFactory = LoggerFactory.Create(builder => builder.AddDebug()
-                                                                             .SetMinimumLevel(MinimumLogLevel));
-            //TODO ? _globalAppLogger = _loggerFactory?.CreateLogger("main");
-
-            // windows is activated, so start (asynchronously run) the MVU program
-            await runMvuProgramAsync(externalMessageDispatcher,
-                                     replaceViewAction: view => replaceMvuComponents(window.MvuComponentContainer, view),
-                                     loggerFactory);
-
-            // the MVU program has terminated normally, so signal the window to close
-            window.Close();
-         }
-         catch (Exception exception) {
-            // TODO !!! _globalAppLogger?.LogError(exception, "General exception while running MVU program");
-            // TODO: form.Close() ?
-         }
-      }
-
-      void onClosedStopMvuProgram(object sender, WindowEventArgs args) {
-         // window is closing, so signal the MVU program to terminate
-         externalMessageDispatcher.Dispatch(MvuMessages.Request_Quit());
-      }
-
-      window.Activated += onActivatedRunMvuProgram;
-      window.Closed    += onClosedStopMvuProgram;
-   }
-
-
-   private static async Task runMvuProgramAsync(ExternalMessageDispatcher? externalMessageDispatcher, Action<PlatformView<ProgramView>> replaceViewAction,
-                                                ILoggerFactory? loggerFactory) {
       ILogger? servicesLogger = loggerFactory?.CreateLogger("svcs");
       ILogger? uiLogger = loggerFactory?.CreateLogger("ui");
       ILogger? programLogger = loggerFactory?.CreateLogger("prog");
+
       IAppServices appServices = new AppServices_Real(servicesLogger);
 
-      PlatformView<ProgramView> view(MvuMessageDispatchDelegate dispatch, Model model)
-         => ViewBuilder.BuildViewFromModel(dispatch, model, uiLogger);
-
-      MvuProgramComponent<Model, PlatformView<ProgramView>> mvuComponent = Component.GetAsComponent(appServices, view, programLogger, loggerFactory);
-
-      // var programRunnerWithServices = ProgramRunnerWithServices<PlatformView<ProgramView>>.Build(externalMessageDispatcher, loggerFactory);
-
-      // run the program until it terminates
-      var finalModel = await ProgramRunnerWithBus.RunProgramWithCommonBusAsync(mvuComponent.BuildProgramRunner,
-                                                                               mvuComponent.BuildProgram,
-                                                                               replaceViewAction,
-                                                                               loggerFactory,
-                                                                               externalMessageDispatcher,
-                                                                               mvuComponent.ProgramInfo,
-                                                                               mvuComponent.MessageAsCommandFunc,
-                                                                               mvuComponent.ExecuteEffectDelegate,
-                                                                               mvuComponent.IsQuitMessageFunc);
+      new MainWindow()
+           .StartMvuProgramInWindow(() => MvuMessages.Request_Quit(),
+                                    () => Component.GetAsComponent(appServices,
+                                                                   (dispatch, model) => ViewBuilder.BuildViewFromModel(dispatch, model, uiLogger),
+                                                                   programLogger,
+                                                                   // ReSharper disable once AccessToDisposedClosure
+                                                                   loggerFactory),
+                                    loggerFactory);
    }
-
-
-   private static void replaceMvuComponents(Panel componentContainer, PlatformView<ProgramView> view) {
-      componentContainer.Children.Clear();
-      foreach (UIElement uiElement in view.MvuView.Controls) {
-         componentContainer.Children.Add(uiElement);
-      }
-   }
-
 }
