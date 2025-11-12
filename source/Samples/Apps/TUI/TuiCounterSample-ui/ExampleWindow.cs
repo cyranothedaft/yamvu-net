5db2b18b826af5673129c7961be0cf810fe84a93
cyranothedaft <cyranothedev@gmail.com>
2025-11-12 13:44:31 -0500

    initial tui sample app

---
+++ source/Samples/Apps/TUI/TuiCounterSample-ui/ExampleWindow.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using Terminal.Gui.App;
+using Terminal.Gui.Configuration;
+using Terminal.Gui.ViewBase;
+using Terminal.Gui.Views;
+
+
+namespace TuiCounterSample.ui;
+
+// Defines a top-level window with border and title
+public class ExampleWindow : Window {
+   public static string UserName { get; set; }
+
+
+   public ExampleWindow() {
+      Title = $"Example App ({Application.QuitKey} to quit)";
+
+      // Create input components and labels
+      var usernameLabel = new Label { Text = "Username:" };
+
+      var userNameText = new TextField
+                            {
+                               // Position text field adjacent to the label
+                               X = Pos.Right(usernameLabel) + 1,
+
+                               // Fill remaining horizontal space
+                               Width = Dim.Fill()
+                            };
+
+      var passwordLabel = new Label
+                             {
+                                Text = "Password:", X = Pos.Left(usernameLabel), Y = Pos.Bottom(usernameLabel) + 1
+                             };
+
+      var passwordText = new TextField
+                            {
+                               Secret = true,
+
+                               // align with the text box above
+                               X     = Pos.Left(userNameText),
+                               Y     = Pos.Top(passwordLabel),
+                               Width = Dim.Fill()
+                            };
+
+      // Create login button
+      var btnLogin = new Button
+                        {
+                           Text = "Login",
+                           Y    = Pos.Bottom(passwordLabel) + 1,
+
+                           // center the login button horizontally
+                           X         = Pos.Center(),
+                           IsDefault = true
+                        };
+
+      // When login button is clicked display a message popup
+      btnLogin.Accepting += (s, e) => {
+                               if (userNameText.Text == "admin" && passwordText.Text == "password") {
+                                  MessageBox.Query("Logging In", "Login Successful", "Ok");
+                                  UserName = userNameText.Text;
+                                  Application.RequestStop();
+                               }
+                               else {
+                                  MessageBox.ErrorQuery("Logging In", "Incorrect username or password", "Ok");
+                               }
+
+                               // When Accepting is handled, set e.Handled to true to prevent further processing.
+                               e.Handled = true;
+                            };
+
+      // Add the views to the Window
+      Add(usernameLabel, userNameText, passwordLabel, passwordText, btnLogin);
+
+      ListView lv = new ListView()
+                       {
+                          Y      = Pos.AnchorEnd(),
+                          Height = Dim.Auto(),
+                          Width  = Dim.Auto()
+                       };
+      lv.SetSource(["One", "Two", "Three", "Four"]);
+      Add(lv);
+   }
+
+
+   public override void EndInit() {
+      base.EndInit();
+
+      // Set the theme to "Anders" if it exists, otherwise use "Default"
+      ThemeManager.Theme = ThemeManager.GetThemeNames().FirstOrDefault(x => x == "Anders") ?? "Default";
+   }
+}
---
+++ source/Samples/Apps/TUI/TuiCounterSample-ui/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Terminal.Gui.App;
+using Terminal.Gui.Configuration;
+
+
+namespace TuiCounterSample.ui;
+
+static class EntryPoint {
+   private static void Main(string[] args) {
+
+      // Override the default configuration for the application to use the Light theme
+      //ConfigurationManager.RuntimeConfig = """{ "Theme": "Light" }""";
+      ConfigurationManager.Enable(ConfigLocations.All);
+
+
+
+      Application.Run<ExampleWindow>().Dispose();
+
+      // Before the application exits, reset Terminal.Gui for clean shutdown
+      Application.Shutdown();
+
+      // To see this output on the screen it must be done after shutdown,
+      // which restores the previous screen.
+      Console.WriteLine($"Username: {ExampleWindow.UserName}");
+   }
+}
